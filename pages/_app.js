ffea7b20319bb12bdd5ee192a76c89255d0a1d69
WeeMellowToby <69983951+WeeMellowToby@users.noreply.github.com>
2022-06-14 15:48:27 +0100

    added meta tag to allow lights to be turned on and off

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,12 @@ import '../styles/globals.css'
 import 'regenerator-runtime/runtime'
 import SideBar from '../components/SideBar';
 import React from 'react';
+import Head from 'next/head';
 function MyApp({ Component, pageProps }) {
   return <>
+  <Head>
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+  </Head>
   <SideBar/>
   <Component {...pageProps} />
   </>
